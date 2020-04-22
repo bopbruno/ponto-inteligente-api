e3b9f4ddc12a9f3247fd8090b254ab0d6c18325e
Bruno Pedrosa <bop_bruno@hotmail.com>
2020-04-22 14:02:34 -0300

    ajuste teste

---
+++ src/main/java/com/pedrosa/pontointeligente/api/repositories/EmpresaRepository.java
@@ -0,0 +1,12 @@
+package com.pedrosa.pontointeligente.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.pedrosa.pontointeligente.api.entities.Empresa;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long>{
+
+	@Transactional(readOnly = true)
+	Empresa findByCnpj(String cnpj);
+}
