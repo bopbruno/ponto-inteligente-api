763d57c628938da20c5cc3e74e931dc1eeed8f1d
Bruno Pedrosa <bop_bruno@hotmail.com>
2020-04-26 19:17:26 -0300

    add CadastroPJController - method cadastrar

---
+++ src/main/java/com/pedrosa/pontointeligente/api/controllers/CadastroPJController.java
@@ -0,0 +1,96 @@
+package com.pedrosa.pontointeligente.api.controllers;
+
+import java.security.NoSuchAlgorithmException;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pedrosa.pontointeligente.api.services.EmpresaService;
+import com.pedrosa.pontointeligente.api.services.FuncionarioService;
+import com.pedrosa.pontointeligente.api.utils.PasswordUtils;
+import com.pedrosa.pontointeligente.api.dtos.CadastroPJDto;
+import com.pedrosa.pontointeligente.api.entities.Empresa;
+import com.pedrosa.pontointeligente.api.entities.Funcionario;
+import com.pedrosa.pontointeligente.api.enums.PerfilEnum;
+import com.pedrosa.pontointeligente.api.response.Response;
+
+@RestController
+@RequestMapping("/api/cadastro-pj")
+public class CadastroPJController {
+
+	private static final Logger log = LoggerFactory.getLogger(CadastroPJController.class);
+	
+	@Autowired
+	private FuncionarioService funcionarioService;
+	
+	@Autowired
+	private EmpresaService empresaService;
+	
+	public CadastroPJController() {}
+	
+	@PostMapping
+	public ResponseEntity<Response<CadastroPJDto>> cadastrar(@Valid @RequestBody CadastroPJDto cadastroPJDto,
+			BindingResult result) throws NoSuchAlgorithmException{
+		log.info("Cadastrando PJ: {}", cadastroPJDto.toString());
+		Response<CadastroPJDto> response = new Response<CadastroPJDto>();
+		
+		validarDadosExistentes(cadastroPJDto, result);
+		Empresa empresa = this.converterDtoParaEmpresa(cadastroPJDto, result);
+		Funcionario funcionario = this.converterDtoParaFuncionario(cadastroPJDto, result);
+		
+		if(result.hasErrors()) {
+			log.error("Erro validando dados de cadastro PJ: {}", result.getAllErrors());
+			result.getAllErrors().forEach(error -> response.getErros().add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		this.empresaService.persistir(empresa);
+		funcionario.setEmpresa(empresa);
+		this.funcionarioService.persistir(funcionario);
+		
+		response.setData(this.converterCadastroPjDto(funcionario));
+		return ResponseEntity.ok(response);
+	}
+
+	private CadastroPJDto converterCadastroPjDto(Funcionario funcionario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Empresa converterDtoParaEmpresa(CadastroPJDto cadastroPJDto, BindingResult result) {
+		Empresa empresa = new Empresa();
+		empresa.setCnpj(cadastroPJDto.getCnpj());
+		empresa.setRazaoSocial(cadastroPJDto.getRazaoSocial());
+		return empresa;
+	}
+
+	private Funcionario converterDtoParaFuncionario(CadastroPJDto cadastroPJDto, BindingResult result) 
+		throws NoSuchAlgorithmException {
+		Funcionario funcionario = new Funcionario();
+		funcionario.setNome(cadastroPJDto.getNome());
+		funcionario.setEmail(cadastroPJDto.getEmail());
+		funcionario.setCpf(cadastroPJDto.getCpf());
+		funcionario.setPerfil(PerfilEnum.ROLE_ADMIN);
+		funcionario.setSenha(PasswordUtils.gerarBCrypt(cadastroPJDto.getSenha()));
+		return funcionario;
+	}
+
+	private void validarDadosExistentes(@Valid CadastroPJDto cadastroPJDto, BindingResult result) {
+		this.empresaService.buscarPorCnpj(cadastroPJDto.getCnpj())
+				.ifPresent(func -> result.addError(new ObjectError("empresa", "Empresa já existe.")));
+		this.funcionarioService.buscarPorCpf(cadastroPJDto.getCpf())
+				.ifPresent(func -> result.addError(new ObjectError("funcionario","CPF já existente.")));
+		this.funcionarioService.buscarPorEmail(cadastroPJDto.getEmail())
+				.ifPresent(func -> result.addError(new ObjectError("funcionario", "Email já existe.")));
+	}
+}
